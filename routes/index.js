c29d17469ecdb86f928d215ddc219366212b9b2b
Dante Miron <dantemiron@hotmail.com>
2023-02-02 16:11:53 -0300

    configurando crud

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,11 @@ router.get('/', index.index)
 router.post('/crear',
 [
     check('jugador').not().isEmpty().withMessage('El campo jugador es requerido'),
-    check('edad').not().isEmpty().withMessage('El campo jugador es requerido'),
-    check('altura').not().isEmpty().withMessage('El campo jugador es requerido'),
-    check('nacionalidad').not().isEmpty().withMessage('El campo jugador es requerido'),
-    check('goles').not().isEmpty().withMessage('El campo jugador es requerido'),
-    check('disponible').not().isEmpty().withMessage('El campo jugador es requerido')
+    check('edad').not().isEmpty().withMessage('El campo edad es requerido'),
+    check('altura').not().isEmpty().withMessage('El campo altura es requerido'),
+    check('nacionalidad').not().isEmpty().withMessage('El nacionalidad jugador es requerido'),
+    check('goles').not().isEmpty().withMessage('El campo goles es requerido'),
+    check('disponible').not().isEmpty().withMessage('El campo disponible es requerido')
 ], index.crearJugador)
 router.get('/ver', index.verJugadores)
 router.get('/ver/:id', validarId, index.verJugador)
