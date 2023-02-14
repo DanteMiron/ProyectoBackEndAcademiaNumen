56633e2844eddf34a4965b42050dd3c323d3ff07
Dante Miron <dantemiron@hotmail.com>
2023-02-14 15:49:17 -0300

    terminando login y logout

---
+++ middlewares/validacionRegister.js
@@ -0,0 +1,10 @@
+const {check} = require('express-validator')
+
+const validacionRegister = [
+    check('name').not().isEmpty().withMessage('El campo jugador es requerido'),
+    check('email').not().isEmpty().withMessage('El campo edad es requerido').isEmail().withMessage('coloque un email válido'),
+    check('password').not().isEmpty().withMessage('El campo altura es requerido')
+    
+];
+
+module.exports = {validacionRegister};
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const {validarId} = require('../middlewares/validarId');
 const {validacionCrearJugador} = require('../middlewares/validacionCrearJugador');
 const {validacionEditarJugador} = require('../middlewares/validacionEditarJugador');
 const auth = require('../middlewares/auth');
+const { validacionRegister } = require('../middlewares/validacionRegister');
+const { validacionLogin } = require('../middlewares/validacionLogin');
 
 
 
@@ -19,10 +21,13 @@ router.get('/verCookie', index.verCookie);
 router.get('/eliminarCookie', index.eliminarCookie);
 
 router.post('/crear',validacionCrearJugador , index.crearJugador);
+router.post('/register', validacionRegister,index.register);
+router.post('/login', validacionLogin, index.login)
 
 router.put('/editar/:id', validacionEditarJugador,index.editarJugador);
 
 router.delete('/eliminar/:id', index.eliminarJugador);
+router.delete('/logout', index.logout)
 
 
 module.exports = router;
